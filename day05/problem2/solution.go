85520c9525852249711c5cc314c2f1c598047d0e
Kyle Johnson <miningape@gmail.com>
2024-12-06 00:11:31 +0100

    chore: moved for loop out of file

--- day05/problem2/solution.go
+++ day05/problem2/solution.go
@@ -7,30 +7,24 @@ import (
 	"sort"
 )
 
-func calculateMiddleOfIncorrectlyOrderedUpdates(rules map[int]util.Set[int], updates [][]int) int {
-	sum := 0
-
-	for _, update := range updates {
-		if day05.IsCorrectlyOrdered(update, rules) {
-			continue
-		}
-		
-		sort.Slice(update, func(i, j int) bool {
-			left := update[i]
-			right := update[j]
-			rule, found := rules[left]
-
-			if found && rule.Contains(right) {
-				return true
-			}
-
-			return false
-		})
-
-		sum += day05.FindMiddle(update)
+func calculateMiddleOfIncorrectlyOrderedUpdate(rules map[int]util.Set[int], update []int) int {
+	if day05.IsCorrectlyOrdered(update, rules) {
+		return 0
 	}
 
-	return sum
+	sort.Slice(update, func(i, j int) bool {
+		left := update[i]
+		right := update[j]
+		rule, found := rules[left]
+
+		if found && rule.Contains(right) {
+			return true
+		}
+
+		return false
+	})
+
+	return day05.FindMiddle(update)
 }
 
 type Day5Solution2 struct {}
@@ -39,7 +33,10 @@ func (Day5Solution2) Solve(path string) {
 	file := util.ReadFile(path)
 	rules, updates := day05.ParseInput(file)
 
-	sum := calculateMiddleOfIncorrectlyOrderedUpdates(rules, updates)
+	sum := 0
+	for _, update := range updates {
+		sum += calculateMiddleOfIncorrectlyOrderedUpdate(rules, update)
+	}
 
 	fmt.Println(sum)
 }
